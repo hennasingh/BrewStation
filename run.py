231d55f1d5847f736b1083f6f61c00a1261d6e76
Henna Singh <hennasingh04@gmail.com>
2024-08-26 12:08:27 +0000

    Add rich text and ascii art packages and print  art

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from rich.console import Console 
+from art import *
+
+console = Console()
+
+tprint("Arya's BrewStation", font="bulbhead")
+console.print(r"""
+        /~~~~~~~~~~~~~~~~~~~/|                   {
+       /              /######/ / |       }    }    {  
+      /              /______/ /  |      {    {  }   }
+     ========================= /||        }   }{   { 
+     |_______________________|/ ||       {  }{   }   }
+      |  \****/     \__,,__/    ||      ( }{ }{    { )
+      |===\**/       __,,__     ||     .-{   }     }-.
+      |______________\====/%____||    ( ( } { }   { } ) 
+      |   ___        /~~~~\ %  / |    |'-.._______..-'|
+     _|  |===|===   /      \%_/  |    |               ;--.
+    | |  |###|     |########| | /     |              (__  \
+    |____\###/______\######/__|/      |               | )  )
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~        \               (   /
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~        '-.._______..-'  ''  
+    """, style="bold yellow")
+
+console.print("""
+    Welcome to Arya's BrewStation! Whether you're a coffee newbie or a seasoned 
+    sipper, BrewStation turns every brew into an adventure. 
+    Ready to pour your passion into the perfect cup? Let’s get brewing! 
+    """, style="bold green")
+
