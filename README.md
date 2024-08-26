949ffcd0084c3085c933660172560eee43eb4f33
Henna Singh <hennasingh04@gmail.com>
2024-08-26 12:10:55 +0000

    Add starting machine stats and possible actions

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### Credits
 - [ascii art](https://ascii.co.uk/art/coffee) - For Coffee ASCII Art
 - [Stack Overflow](https://stackoverflow.com/questions/52335970/how-to-fix-syntaxwarning-invalid-escape-sequence-in-python - For correcting invalid excape sequence warning)
+- [Rich Text](https://pypi.org/project/rich/) - For adding colors to text
 
 
 
--- run.py
+++ run.py
@@ -27,3 +27,53 @@ console.print("""
     Ready to pour your passion into the perfect cup? Let’s get brewing! 
     """, style="bold green")
 
+status = True
+
+machine_status = {
+    'water':400,
+    'milk' : 540,
+    'coffee': 120,
+    'cups': 9,
+    'money': 550
+}
+
+def machine_stats():
+    """
+    The function prints the ingredients available in the machine
+    """
+    console.print("The coffee machine has:")
+    console.print(f"{machine_status['water']} ml of water")
+    console.print(f"{machine_status['milk']} ml of milk")
+    console.print(f"{machine_status['coffee']} g of coffee beans")
+    console.print(f"{machine_status['cups']} disposable cups")
+    console.print(f"${machine_status['money']} of money")
+
+
+def check_machine_action(action):
+    global status
+
+    if action == "buy":
+        pass
+    elif action == "fill":
+        fill_machine_ingredients()
+    elif action == "take":
+        pass
+    elif action == 'remaining':
+        machine_stats()
+    elif action == 'exit':
+        status = False
+    else:
+        print("Invalid action")
+
+
+def start_coffee_machine():
+    """
+    The function that starts the machine and pass the input action
+    """
+    while status:
+        action = input(f"Write action (buy, fill, take, remaining, exit):")
+        check_machine_action(action.lower())
+
+
+start_coffee_machine()
+
